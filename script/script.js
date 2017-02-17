da05f3fdde7e2f5367fecdd09ea3f015bea2f2bc
hikarireika <saradesouza87@gmail.com>
2017-02-17 23:58:42 +0800

    Working prototype

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	$('.dice').click(function(){
+
+		//find out what type of dice this is
+		var currentDice = $(this).attr('value');
+		console.log(currentDice);
+
+		//create a random number with its max the value of the dice
+		var randomNumber = Math.floor(Math.random() * currentDice) + 1
+
+		//send the output to the screen
+		$('#output').text("Your roll is: " + randomNumber);
+
+	});
+
+});
