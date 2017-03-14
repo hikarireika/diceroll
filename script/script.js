4d3bbf3e153992a187ddc54e91a84b379209a1bd
hikarireika <saradesouza87@gmail.com>
2017-03-14 23:41:52 +0800

    major update: reskin, change 1 rolls to red

--- script/script.js
+++ script/script.js
@@ -1,22 +1,54 @@
 $(document).ready(function(){
 
 	 $('.diceslider').slick({
-	 	infinite: false,
-	 	dots: false,
-	 	arrows: false,
+	 	 infinite: false,
+		 dots: true,
+		 accessibility: true,
+		 arrows: false,
+		speed: 200,
+		swipe: false,
 	  });
+
+	 // $('#btn-left').slickPrev();
+
+
+	 $('#btn-right').click(function(){
+
+	 	$('.diceslider').slick('slickNext');
+	 
+	 });
+	
+	$('#btn-left').click(function(){
+
+	 	$('.diceslider').slick('slickPrev');
+	 
+	 });
+
 
 	$('.dice').click(function(){
 
 		//find out what type of dice this is
 		var currentDice = $(this).attr('value');
-		console.log(currentDice);
+		var badRoll = "1"
+		// console.log(currentDice);
+
+		
 
 		//create a random number with its max the value of the dice
-		var randomNumber = Math.floor(Math.random() * currentDice) + 1
+		var randomNumber = Math.floor(Math.random() * currentDice) + 1;
+
+		if (randomNumber == badRoll) {
+			console.log("it's one");
+			$('#result').css( "color", "#dc3838" );
+		} else {
+			console.log("not the one");
+			$('#result').css( "color", "#18495b" );
+		}
+
 
 		//send the output to the screen
-		$('#output').text("Your roll is: " + randomNumber);
+		$('#result').hide().fadeIn(500).text("You rolled: " + randomNumber);
+
 
 	});
 
